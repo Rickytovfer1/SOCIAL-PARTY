e373c5b513ad9abb665c3e9063c4b3157631da75
GGSafa256 <ggomezmonasterio@safareyes.es>
2025-01-22 23:41:28 +0100

    imagenes,cambios css.

--- Frontend-Social-Party/src/app/services/user.service.ts
+++ Frontend-Social-Party/src/app/services/user.service.ts
@@ -9,8 +9,8 @@ export class UserService {
     {
       id: 1,
       name: 'Usuario 1',
-      profileImage: 'assets/images/users/1.png',
-      bio: 'Usuario 1',
+      profileImage: 'assets/users/1.png',
+      bio: 'bio de usuario 1',
       instagram: 'https://instagram.com/usuario1',
       tiktok: 'https://tiktok.com/usuario1',
       bereal: 'https://bere.al/usuario1',
@@ -18,15 +18,15 @@ export class UserService {
     {
       id: 2,
       name: 'Usuario 2',
-      profileImage: 'assets/images/users/1.png',
-      bio: 'Usuario 2',
+      profileImage: 'assets/users/1.png',
+      bio: 'bio de usuario 2',
       instagram: 'https://instagram.com/usuario2',
     },
     {
       id: 3,
       name: 'Usuario 3',
-      profileImage: 'assets/images/users/1.png',
-      bio: 'Usuario 3',
+      profileImage: 'assets/users/1.png',
+      bio: 'bio de usuario 3',
       tiktok: 'https://tiktok.com/@usuario3',
       bereal: 'https://bere.al/usuario3',
 
