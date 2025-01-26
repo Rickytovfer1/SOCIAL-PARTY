2910abb4b47a5a1e9fcc8ef3615344056a566a0c
JoseCarlos05 <jgomezbotana@safareyes.es>
2025-01-26 17:48:04 +0100

    navs comenzados

--- Frontend-Social-Party/src/app/nav-inferior/nav-inferior.component.ts
+++ Frontend-Social-Party/src/app/nav-inferior/nav-inferior.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-nav-inferior',
   templateUrl: './nav-inferior.component.html',
   styleUrls: ['./nav-inferior.component.scss'],
   standalone: true,
+  imports: [
+    IonicModule
+  ]
 })
 export class NavInferiorComponent  implements OnInit {
 
--- Frontend-Social-Party/src/app/nav-lateral/nav-lateral.component.ts
+++ Frontend-Social-Party/src/app/nav-lateral/nav-lateral.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-nav-lateral',
   templateUrl: './nav-lateral.component.html',
   styleUrls: ['./nav-lateral.component.scss'],
   standalone: true,
+  imports: [
+    IonicModule
+  ]
 })
 export class NavLateralComponent  implements OnInit {
 
