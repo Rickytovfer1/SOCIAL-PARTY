43a9266afe635381292052f4a1f0071969b7b97e
JoseCarlos05 <jgomezbotana@safareyes.es>
2025-01-27 08:44:42 +0100

    Lógica de inicio de sesión  comenzada, icono arreglado

--- Frontend-Social-Party/src/app/login/login.component.ts
+++ Frontend-Social-Party/src/app/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {IonicModule} from "@ionic/angular";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+import {Login} from "../modelos/Login";
+import {Registro} from "../modelos/Registro";
 
 @Component({
   selector: 'app-login',
@@ -12,8 +16,47 @@ import {IonicModule} from "@ionic/angular";
 })
 export class LoginComponent  implements OnInit {
 
-  constructor() { }
+  login: Login = new Login();
+  loginViewFlag: boolean = true;
+
+  constructor(private router: Router, private fb: FormBuilder) { }
 
   ngOnInit() {}
+
+  // doLogin(): void {
+  //
+  //   if (this.loginForm.valid) {
+  //     this.login = {...this.login, ...this.loginForm.value};
+  //     this.loginService.loguear(this.login).subscribe({
+  //       next: (respuesta) => {
+  //         const token = respuesta.token; // Accede al token
+  //         sessionStorage.setItem("authToken", token);
+  //
+  //         // Notificar sobre el cambio en el estado de autenticación
+  //         this.loginService.setAuthState(true);
+  //
+  //       },
+  //       error: (e) => console.error(e),
+  //       complete: () => this.router.navigate([''])
+  //     })
+  //
+  //
+  //   } else {
+  //     console.log('Formulario inválido. Por favor verifica los datos.');
+  //   }
+  //
+  // }
+  //
+  // goRegister() {
+  //   this.loginViewFlag = false;
+  //   this.ngOnInit()
+  //
+  //
+  // }
+  //
+  // goLogin() {
+  //   this.loginViewFlag = true;
+  //   this.ngOnInit()
+  // }
 
 }
