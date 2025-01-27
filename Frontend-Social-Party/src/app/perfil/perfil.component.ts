342da8f2eb1f352ab3d992320b60b58abd068126
Ricardo <rtovarfernandez@safareyes.es>
2025-01-27 13:06:33 +0100

    empezado get perfil

--- Frontend-Social-Party/src/app/perfil/perfil.component.ts
+++ Frontend-Social-Party/src/app/perfil/perfil.component.ts
@@ -1,23 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IonicModule} from "@ionic/angular";
 import {NavSuperiorComponent} from "../nav-superior/nav-superior.component";
 import {NavInferiorComponent} from "../nav-inferior/nav-inferior.component";
+import {ActivatedRoute} from "@angular/router";
+import {Perfil} from "../modelos/Perfil";
+import {PerfilServicio} from "../servicios/perfil.service";
 
 @Component({
-  selector: 'app-perfil',
-  templateUrl: './perfil.component.html',
-  styleUrls: ['./perfil.component.scss'],
-  standalone: true,
-  imports: [
-    IonicModule,
-    NavSuperiorComponent,
-    NavInferiorComponent,
-  ]
+    selector: 'app-perfil',
+    templateUrl: './perfil.component.html',
+    styleUrls: ['./perfil.component.scss'],
+    standalone: true,
+    imports: [
+        IonicModule,
+        NavSuperiorComponent,
+        NavInferiorComponent,
+    ]
 })
-export class PerfilComponent  implements OnInit {
+export class PerfilComponent implements OnInit {
 
-  constructor() { }
+    perfil: Perfil = {} as Perfil;
+    id!:number;
 
-  ngOnInit() {}
+    constructor(private perfilService: PerfilServicio,
+                private activateRoute: ActivatedRoute) {
+    }
 
+    ngOnInit() {
+        this.cargarPerfil(10);
+
+    }
+
+    cargarPerfil(idUsuario: number): void {
+        this.perfilService.getPerfil(idUsuario).subscribe({
+                next: (perfil: Perfil) => {
+                    this.perfil = perfil;
+                },
+                error: (e) => {
+                    console.error("Error");
+                },
+                complete: () =>
+                    console.info(this.perfil)
+
+            }
+        );
+    }
 }
