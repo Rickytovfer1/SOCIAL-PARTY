fbc4a1094cd47ce576fe4014b0cbcd40ac8b59fe
GGSafa256 <ggomezmonasterio@safareyes.es>
2025-01-29 10:12:06 +0100

    Ver empresas conectado al backend, empresa.service creado y ver empresas html y ts modificados.

---
+++ Frontend-Social-Party/src/app/servicios/empresa.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from "../../environments/environment";
+import { ComunService } from "./comun.service";
+
+export interface EmpresaDTO {
+    id: number;
+    nombre: string;
+    direccion: string;
+    cp: string;
+    nif: string;
+    telefono: string;
+    valoracionMinima: number;
+    edadMinima: number;
+    eventos: number[];
+    idUsuario: number;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EmpresaService {
+    private baseUrl = environment.apiUrl;
+
+    constructor(private http: HttpClient, private comunService: ComunService) { }
+
+    listarEmpresas(): Observable<EmpresaDTO[]> {
+        const options = this.comunService.autorizarPeticion();
+        return this.http.get<EmpresaDTO[]>(`${this.baseUrl}/cliente/ver/empresas`, options)
+            .pipe(
+                catchError(error => {
+                    console.error('Error fetching empresas:', error);
+                    return throwError(() => new Error('Error fetching empresas'));
+                })
+            );
+    }
+}
