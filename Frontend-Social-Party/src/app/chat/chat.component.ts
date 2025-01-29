cf78e6db2894750eeb379b1cde41b2ba410cd1ab
JoseCarlos05 <jgomezbotana@safareyes.es>
2025-01-29 12:24:47 +0100

    Chat casi terminado en estilo

---
+++ Frontend-Social-Party/src/app/chat/chat.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {IonicModule} from "@ionic/angular";
+import {NavInferiorComponent} from "../nav-inferior/nav-inferior.component";
+import {NavSuperiorComponent} from "../nav-superior/nav-superior.component";
+
+@Component({
+    selector: 'app-chat',
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.scss'],
+    standalone: true,
+    imports: [
+        IonicModule,
+        NavInferiorComponent,
+        NavSuperiorComponent
+    ]
+})
+export class ChatComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
