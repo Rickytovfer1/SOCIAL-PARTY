86ed865ac5d983cd7ee21f6b3e10208be9fc976b
Ricardo <rtovarfernandez@safareyes.es>
2025-01-31 10:14:09 +0100

    ver eventos hecho

---
+++ Frontend-Social-Party/src/app/modelos/Evento.ts
@@ -0,0 +1,10 @@
+export interface Evento{
+    id: number;
+    horaApertura?: string;
+    horaFinalizacion?: string;
+    fecha?: string;
+    titulo?: string;
+    foto: string;
+    descripcion?: string;
+    idReceptor?: number;
+}
